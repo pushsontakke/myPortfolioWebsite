6580abf1f0e8fd64ff5d81806c97f34ff0e7d5cb
pushsontakke <pusontakke1@gmail.com>
2026-04-02 15:48:42 +0000

    remove: unnecessary commented code removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,13 +40,3 @@ export default function Home() {
     </div>
   );
 }
-
-// export default function Home() {
-//   return (
-//     <div className="my-28">
-//     <div className="text-center text-8xl font-bold text-yellow-500">Portfolio | Piyush Sontakke
-//   </div>
-//     <div className="text-center text-7xl text-yellow-300">Product Engineer who ships - not just codes</div>
-//     </div>
-//   );
-// }
